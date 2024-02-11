a67eeeb65dc49db83749286941afda412f27a1ac
Martin Weber Nissle <webernissle@gmail.com>
2024-02-11 23:10:31 +0100

    working on win and mac

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # m5core-esp32
 M5STACK core esp32
+
+Install Board M5Stack Core ESP32
+compiler esptool
+
+C_CPP config set Mac and Win32
+
+Install libraries M5Stack
+
+Set in Board Manager upload speed to 115000
+
--- hello/hello.ino
+++ hello/hello.ino
@@ -6,11 +6,21 @@ void setup()
 {
   M5.begin();  //Init M5Core.
   M5.Power.begin(); //Init Power module.
-  M5.Lcd.print("Hello World");  // Print text on the screen (string)
+  // print text on lcd with big letters
+  M5.Lcd.setTextSize(3);  // Set the text size to 3 times the default size
+  M5.Lcd.print("Hello Martin");  // Print text on the screen (string)
+  
 }
 
 // After the program in setup() runs, it runs the program in loop()
 // The loop() function is an infinite loop in which the program runs repeatedly
 void loop()
 {
+  M5.update();  // Update button status
+  // capture button A pressed
+  if (M5.BtnA.wasPressed())
+  {
+    M5.Lcd.clear();  // Clear the screen
+    M5.Lcd.print("Button A pressed");  // Print text on the screen (string)
+  }
 }
