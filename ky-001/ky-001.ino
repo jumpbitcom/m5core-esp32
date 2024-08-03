7ff60b732fc835f00c6fd0e94da4d4eb53a3c654
Martin Weber Nissle <webernissle@gmail.com>
2024-08-03 22:19:33 +0200

    wifi and menu with buttons

--- ky-001/ky-001.ino
+++ ky-001/ky-001.ino
@@ -1,45 +1,135 @@
 // Required libraries will be imported
 #include <OneWire.h>
-#include <DallasTemperature.h>            
+#include <DallasTemperature.h>
 #include <M5Stack.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
 
 // Here the input pin is declared to which the sensor module is connected
 #define KY001_Signal_PIN 19 // GPIO2 IO6
 
+// WiFi credentials
+const char *ssid = "iPhone";
+const char *password = "2pac-1234";
+
 // Libraries are configured
-OneWire oneWire(KY001_Signal_PIN);          
-DallasTemperature sensors(&oneWire);    
+OneWire oneWire(KY001_Signal_PIN);
+DallasTemperature sensors(&oneWire);
 
+void setup()
+{
+  M5.begin(); // Init M5Core.
+  WiFi.begin(ssid, password);
 
-void setup() {
-  M5.begin();  //Init M5Core.
-  M5.Power.begin(); //Init Power module.
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+
+  M5.Power.begin(); // Init Power module.
   // print text on lcd with big letters
-  M5.Lcd.setTextSize(3);  // Set the text size to 3 times the default size
-  M5.Lcd.print("KY-001 temp");  // Print text on the screen (string)
-
- // Sensor is initialized
-  sensors.begin();  
+  M5.Lcd.setTextSize(3); // Set the text size to 3 times the default size
+  showMenu();
+  // Sensor is initialized
+  sensors.begin();
 }
 
-//main program loop
+// main program loop
 void loop()
 {
-    // Temperature measurement is started...
-    sensors.requestTemperatures();
-    // M5 get pin RXD1 and output measured Data 
-    M5.Lcd.setCursor(0, 0);  // Set the cursor to the top left corner of the screen
-    
+  // check if the button is pressed
+  M5.update(); // Update button status
+  // capture button A pressed
+  if (M5.BtnA.wasPressed())
+  {
+    M5.Lcd.clear();           // Clear the screen
+    M5.Lcd.setCursor(0, 0);   // Set the cursor to the top left corner of the screen
+    M5.Lcd.print("Button A"); // Print text on the screen (string)
+    // wait 1 second
+    delay(1000);
+    // call function get temperature
+    getTemperature();
+    showMenu();
+  }
+  if (M5.BtnB.wasPressed())
+  {
+    M5.Lcd.clear();           // Clear the screen
+    M5.Lcd.setCursor(0, 0);   // Set the cursor to the top left corner of the screen
+    M5.Lcd.print("Button B"); // Print text on the screen (string)
+    // wait 1 second
+    delay(1000);
+    // call function get temperature
+    getTime();
+    showMenu();
+  }
+}
 
+// Function to get the temperature
+void getTemperature()
+{
+  // Temperature measurement is started...
+  sensors.requestTemperatures();
+  // M5 get pin RXD1 and output measured Data
+  M5.Lcd.clear();             // Clear the screen
+  M5.Lcd.setCursor(0, 0);     // Set the cursor to the top left corner of the screen
+  M5.Lcd.print("Temperatur"); // Print text on the screen (string)
+  M5.Lcd.setCursor(0, 30);    // Set the cursor to the top left corner of the screen
 
-    M5.Lcd.print("T: ");  // Print text on the screen (string)
-        
-    // ... and output measured temperature
-    M5.Lcd.print("T: ");  // Print text on the screen (string)
-    M5.Lcd.print(sensors.getTempCByIndex(0));
+  // ... and output measured temperature
+  M5.Lcd.print("T: "); // Print text on the screen (string)
+  M5.Lcd.print(sensors.getTempCByIndex(0));
+  delay(4000); // 1s pause until next measurement
+}
 
-    delay(1000); // 1s pause until next measurement
-    M5.Lcd.clear();
-    // begin on top of screen to print new temperature
-    M5.Lcd.setCursor(0, 0);
+// function to show time from internet source
+void getTime()
+{
+  M5.Lcd.clear();         // Clear the screen
+  M5.Lcd.setCursor(0, 0); // Set the cursor to the top left corner of the screen
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    HTTPClient http;
+    Serial.println("Do http get request");
+    http.begin("http://worldtimeapi.org/api/timezone/Europe/Zurich");
+    int httpCode = http.GET();
+
+    if (httpCode > 0)
+    {
+      String payload = http.getString();
+      Serial.println(payload);
+
+      // Parse the JSON response to extract the time
+      int timeIndex = payload.indexOf("datetime\":\"") + 11;
+      String timeString = payload.substring(timeIndex, payload.indexOf("\"", timeIndex));
+
+      M5.Lcd.clear();
+      M5.Lcd.setCursor(0, 0);
+      M5.Lcd.print("Time:");
+      M5.Lcd.setCursor(0, 30);
+      M5.Lcd.print(timeString);
+      delay(4000); // 1s pause until next measurement
+    }
+    else
+    {
+      Serial.println("Error on HTTP request");
+    }
+    http.end();
+  }
+  else
+  {
+    Serial.println("WiFi not connected");
+  }
+}
+
+void showMenu()
+{
+  M5.Lcd.clear();                // Clear the screen
+  M5.Lcd.setCursor(0, 0);        // Set the cursor to the top left corner of the screen
+  M5.Lcd.print("Button A Temp"); // Print text on the screen (string)
+  M5.Lcd.setCursor(0, 30);       // Set the cursor to the top left corner of the screen
+  M5.Lcd.print("Button B Time"); // Print text on the screen (string)
+  M5.Lcd.setCursor(0, 60);       // Set the cursor to the top left corner of the screen
+  M5.Lcd.print("Button C IoT");  // Print text on the screen (string)
 }
